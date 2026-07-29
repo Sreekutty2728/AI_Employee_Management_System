e2c83322f5243937ba7be186896f79efceb57e47
Sreekutty K S <sreekutty.ks@vaisesika.local>
2026-07-29 21:41:30 +0530

    describe your changes

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'employee_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ai_employee_management_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Sreekutty@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import EmployeeForm
 
-# Create your views here.
+def add_employee(request):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('add_employee')
+    else:
+        form = EmployeeForm()
+
+    return render(request, 'employees/add_employee.html', {'form': form})
